de024c1c039232dd94c333fe5c90650524bc603d
Nahid Huseynli <nahidhsynli@gmail.com>
2023-10-30 20:17:47 +0400

    Complete task

--- src/models/product.js
+++ src/models/product.js
@@ -1,10 +1,23 @@
-const Sequelize = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 const sequelize = require('../lib/sequelize');
 
-const Product = sequelize.define(
-    'Product',
-    {},
-    {timestamps: true, paranoid: true},
-);
+const Product = sequelize.define('Product', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  stockCount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 
 module.exports = Product;

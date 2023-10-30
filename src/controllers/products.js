cbb4014a78537cbfc1a31014e2b48d885a54e035
EkberAkh <ekber.0303@gmail.com>
2023-10-30 21:30:09 +0400

    solution

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product');
+
 /**
  * @typedef {import('express').Request} express.Request
  * @typedef {import('express').Response} express.Response
@@ -8,35 +10,77 @@
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function createProduct(req, res) {}
+async function createProduct(req, res) {
+  const {title, description, price, stockCount} = req.body;
+  const product = await Product.create({
+    title,
+    description,
+    price,
+    stockCount,
+  });
+  res.status(201).json(product);
+}
 
 /**
  * Updates a product by ID in database
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function updateProduct(req, res) {}
+async function updateProduct(req, res) {
+  const productId = req.params.id;
+  const {title, description, price, stockCount} = req.body;
+  const product = await Product.findByPk(productId);
+
+  if (!product) {
+    return res.status(404).json({error: 'Product not found'});
+  }
+
+  await product.update({title, description, price, stockCount});
+  res.status(200).json(product);
+}
 
 /**
  * Lists all products in database
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function listProducts(req, res) {}
+async function listProducts(req, res) {
+  const products = await Product.findAll();
+  res.status(200).json(products);
+}
 
 /**
  * Get single product by ID
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function getProduct(req, res) {}
+async function getProduct(req, res) {
+  const productId = req.params.id;
+  const product = await Product.findByPk(productId);
+
+  if (!product) {
+    return res.status(404).json({error: 'Product not found'});
+  }
+
+  res.status(200).json(product);
+}
 
 /**
  * Deletes a product by ID
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function deleteProduct(req, res) {}
+async function deleteProduct(req, res) {
+  const productId = req.params.id;
+  const product = await Product.findByPk(productId);
+
+  if (!product) {
+    return res.status(404).json({error: 'Product not found'});
+  }
+
+  await product.destroy();
+  res.status(204).end();
+}
 
 module.exports = {
   createProduct,
--- src/models/product.js
+++ src/models/product.js
@@ -3,7 +3,29 @@ const sequelize = require('../lib/sequelize');
 
 const Product = sequelize.define(
     'Product',
-    {},
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      stockCount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    },
     {timestamps: true, paranoid: true},
 );
 

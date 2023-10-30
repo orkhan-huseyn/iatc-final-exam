4d344d308e7beebc374e5ee1052fff03d2eda274
hazratahmdzd <hazratahmadzada@gmail.com>
2023-10-30 21:17:28 +0400

    Add solutoin

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product');
+
 /**
  * @typedef {import('express').Request} express.Request
  * @typedef {import('express').Response} express.Response
@@ -8,35 +10,76 @@
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function createProduct(req, res) {}
+async function createProduct(req, res) {
+  const {title, description, price, stockCount} = req.body;
+
+  const product = await Product.create({title, description, price, stockCount});
+  return res.status(201).json(product);
+}
 
 /**
  * Updates a product by ID in database
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function updateProduct(req, res) {}
+async function updateProduct(req, res) {
+  const id = req.params.id;
+  const {title, description, price, stockCount} = req.body;
+  const existingProduct = await Product.findByPk(id);
+  if (!existingProduct) {
+    return res.status(404).json({error: 'There is no product'});
+  }
+
+  await existingProduct.update({
+    title: title,
+    description: description,
+    price: price,
+    stockCount: stockCount,
+  });
+  return res.status(200).json(existingProduct);
+}
 
 /**
  * Lists all products in database
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function listProducts(req, res) {}
+async function listProducts(req, res) {
+  const products = await Product.findAll();
+  return res.status(200).json(products);
+}
 
 /**
  * Get single product by ID
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function getProduct(req, res) {}
+async function getProduct(req, res) {
+  const id = req.params.id;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(404).json({error: 'There is no product'});
+  }
+  return res.status(200).json(product);
+}
 
 /**
  * Deletes a product by ID
  * @param {express.Request} req
  * @param {express.Response} res
  */
-async function deleteProduct(req, res) {}
+async function deleteProduct(req, res) {
+  const id = req.params.id;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+    return res.status(404).json({error: 'There is no product'});
+  }
+
+  await product.destroy();
+  return res.status(204).send();
+}
 
 module.exports = {
   createProduct,
